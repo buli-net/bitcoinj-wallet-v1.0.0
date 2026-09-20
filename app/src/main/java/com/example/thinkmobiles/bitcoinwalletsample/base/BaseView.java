1860553d7efd259135889f372f256a2159d726d6
buli <hvlrcb@gmail.com>
2026-09-20 23:17:30 +0700

    Delete app/src/main/java/com/example/thinkmobiles/bitcoinwalletsample/base directory

--- app/src/main/java/com/example/thinkmobiles/bitcoinwalletsample/base/BaseView.java
+++
@@ -1,9 +0,0 @@
-package com.example.thinkmobiles.bitcoinwalletsample.base;
-
-/**
- * Created by Lynx on 4/11/2017.
- */
-
-public interface BaseView<T extends BasePresenter> {
-    void setPresenter(T presenter);
-}
