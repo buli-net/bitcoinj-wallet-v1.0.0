b5b0389e34449764c0992527c6a69340d5186df7
buli <hvlrcb@gmail.com>
2026-09-22 20:29:48 +0700

    Delete app directory

--- app/src/main/java/wallet/history/TransactionItem.java
+++
@@ -1,8 +0,0 @@
-package wallet.history;
-
-public final class TransactionItem {
-    public final String type, amount, time, confirmations, txid;
-    public TransactionItem(String type, String amount, String time, String confirmations, String txid) {
-        this.type = type; this.amount = amount; this.time = time; this.confirmations = confirmations; this.txid = txid;
-    }
-}
