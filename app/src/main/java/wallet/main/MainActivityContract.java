0bab06937cc5dd485412b62b4d85a25206aa9d14
buli <hvlrcb@gmail.com>
2026-09-23 10:46:08 +0700

    Update MainActivityContract.java

--- app/src/main/java/wallet/main/MainActivityContract.java
+++ app/src/main/java/wallet/main/MainActivityContract.java
@@ -8,41 +8,81 @@ import wallet.base.BaseView;
  */
 
 public interface MainActivityContract {
+
     interface MainActivityView extends BaseView<MainActivityPresenter> {
+
         void displayDownloadContent(boolean isShown);
+
         void displayProgress(int percent);
+
         void displayPercentage(int percent);
 
         void displayMyBalance(String myBalance);
+
         void displayTransactionHistory(String history);
-        void displayTransactionHistoryPages(int currentPage, int pageCount);
+
+        void displayTransactionHistoryPages(
+                int currentPage,
+                int pageCount
+        );
+
         void displayWalletPath(String walletPath);
 
         void displayMyAddress(String myAddress);
+
         void displayRecipientAddress(String recipientAddress);
 
         void showToastMessage(String message);
+
         String getRecipient();
+
         String getAmount();
+
         void clearAmount();
+
         int getFeeRateSatPerVkb();
-        void displaySendDetails(String feeRate, String fee, String total, String remaining);
+
+        void displaySendDetails(
+                String feeRate,
+                String fee,
+                String total,
+                String remaining
+        );
+
         android.content.Context getActivityContext();
 
         void startScanQR();
+
         void displayInfoDialog(String myAddress);
+
         void startWalletBackup(String suggestedFileName);
     }
+
     interface MainActivityPresenter extends BasePresenter {
+
+        /*
+         * Required for Activity recreation when the system
+         * Light/Dark theme changes.
+         */
+        void attachView(
+                MainActivityContract.MainActivityView newView
+        );
+
         void refresh();
+
         void selectTransactionHistoryPage(int page);
+
         void pickRecipient();
+
         void send();
 
         void getInfoDialog();
+
         void prepareWalletBackup();
+
         void restoreWallet(android.net.Uri backupUri);
     }
+
     interface MainActivityModel {
 
     }
